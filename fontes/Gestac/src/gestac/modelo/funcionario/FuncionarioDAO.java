b0905f4dec7bbc076479efccb72cf52c90c2818b
ronnimiranda <User@User-PC>
2014-03-08 02:08:58 -0300

    Classe FuncionarioDAO

---
+++ fontes/Gestac/src/gestac/modelo/funcionario/FuncionarioDAO.java
@@ -0,0 +1,22 @@
+package gestac.modelo.funcionario;
+
+import gestac.modelo.veiculos.VeiculoFuncionario;
+
+public class FuncionarioDAO {
+	public static void obterVeiculo(int matricula){
+	//Era pra retornar a List<VeiculoFuncionario> mas nao sabemos como fazer isso,
+	//Entao coloquei como void para nao dar erro.	
+	//by:Ronni	
+		
+	}
+	public static void registrar(Funcionario func) {
+		
+	}
+	public static int obterFuncionario(String login){
+		return obterFuncionario(login);
+	}
+	public static void adicionarVeiculo(Funcionario func,VeiculoFuncionario v){
+		
+	}
+
+}
