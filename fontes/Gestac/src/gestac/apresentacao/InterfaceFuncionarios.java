3a23ddbc3d163ccb59edc29be2a6a251d4111328
gflci <georgiaflci@gmal.com>
2014-03-08 15:41:43 -0300

    Interface funcionario

--- fontes/Gestac/src/gestac/apresentacao/InterfaceFuncionarios.java
+++ fontes/Gestac/src/gestac/apresentacao/InterfaceFuncionarios.java
@@ -1,15 +1,16 @@
 package gestac.apresentacao;
 
+import gestac.modelo.funcionario.ControleFuncionario;
 import gestac.modelo.funcionario.Funcionario;
 
 public class InterfaceFuncionarios {
 	
 	public static void registrarFuncionario() {
-		
+		Funcionario func = new Funcionario();
 	}
 	
-	public static void registrarFuncionario(Funcionario funcionario) {
-		
+	public static void registrarFuncionario(Funcionario func) {
+		ControleFuncionario.registrarFuncionario(func);
 	}
 
 }
